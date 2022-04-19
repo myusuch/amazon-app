4273fb7495c6cf5003f06bee3b75c22a307b5344
David Choi <myusuch@gmail.com>
2022-04-18 21:50:45 -0400

    initial commmit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    label: String,
+    fieldId: {
+      type: String,
+      required: true,
+    },
+    checked: {
+      type: Boolean,
+    },
+  },
+};
+
+</script>
+
+<template>
+ <input type="checkbox" id="checkbox" v-model="checked" />
+	<label for="checkbox">{{ checked }}</label>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
